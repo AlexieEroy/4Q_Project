84933135720817263468995dd634f3b7e43d9638
Rixxa <143713369+AlexieEroy@users.noreply.github.com>
2024-05-24 10:06:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// var firstname = document.getElementById("fname").value;
+// var lastname = document.getElementById("lname").value;
+// var mobile_num = document.getElementById("mobile_num").value;
+// var email = document.getElementById("email").value;
