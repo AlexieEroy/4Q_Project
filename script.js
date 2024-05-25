e43f38cf84b6820ec49ab64625bf36e10546cab6
Rixxa <143713369+AlexieEroy@users.noreply.github.com>
2024-05-25 03:09:04 -0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// var firstname = document.getElementById("fname").value;
-// var lastname = document.getElementById("lname").value;
-// var mobile_num = document.getElementById("mobile_num").value;
-// var email = document.getElementById("email").value;
